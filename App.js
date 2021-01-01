62dd8e499fc65f2aebe1df8a9ce7a123af40a160
saji2000 <48940225+saji2000@users.noreply.github.com>
2020-12-31 23:26:34 -0700

    Finished the tutorial

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
-
 import { View, Image, Text, SafeAreaView, ImageBackground, StyleSheet} from "react-native";
+
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,21 +1,72 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Image } from 'react-native';
+import { 
+    ImageBackground, 
+    StyleSheet, 
+    Image, 
+    View,
+} from 'react-native';
+
+import colors from "../config/colors";
 
 function ViewImageScreen(props) {
     return (
-        <Image
-            source={require("../assets/chair.jpg")}
-            style={styles.image}
+        <View 
+            style={styles.container}
         >
-        </Image>
+            <View
+                style={styles.buttons}
+            >
+                <View style={styles.leftButton}></View>
+                <View style={styles.rightButton}></View>
+            </View>
+            <Image
+            source={require("../assets/chair.jpg")}
+            resizeMode="contain"
+            style={styles.image}
+            >
+            </Image>
+        </View>
+        
     );
 }
 
 const styles = StyleSheet.create({
+
+    buttons:{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        // position: "absolute",
+    },
+
+    container:{
+        backgroundColor: "#000",
+        flex: 1,
+    },
+
     image:{
         width: "100%",
         height: "100%",
+        justifyContent: "center"
     },
+
+    leftButton:{
+        backgroundColor: colors.primary,
+        width: 70,
+        height: 70,
+        top: 40,
+        left: 20,
+    },
+
+    rightButton:{
+        backgroundColor: colors.secondary,
+        width: 70,
+        height: 70,
+        top: 40,
+        right: 20,
+    }
+
+
 })
 
 export default ViewImageScreen;
